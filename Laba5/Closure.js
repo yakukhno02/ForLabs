2c948f39ba8541da7a9ec7885852dc1044a2ca8d
ykmsh <mayak.ol.02.10@gmail.com>
2024-12-04 20:23:29 +0200

    add Lab5(fixed Closure.js)

--- Laba5/Closure.js
+++ Laba5/Closure.js
@@ -10,8 +10,6 @@ const seq = (f) => {
     };
 };
 
-module.exports = { seq };
-
 console.log(seq(x => x + 7)(x => x * 2)(5));
 console.log(seq(x => x * 2)(x => x + 7)(5));
 console.log(seq(x => x + 1)(x => x * 2)(x => x / 3)(x => x - 4)(7));
