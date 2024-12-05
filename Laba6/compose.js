4237927cafb454a8461f04d70b869bfd93f40137
ykmsh <mayak.ol.02.10@gmail.com>
2024-12-06 00:02:25 +0200

    add compose(lab6)

---
+++ Laba6/compose.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const compose = (...fns) => {
+    let handlers = [];
+
+    const composed = (x) => {
+        let result = x;
+        try {
+            for (let i = fns.length - 1; i >= 0; i--) {
+                result = fns[i](result);
+            }
+            return result;
+        } catch (error) {
+            for (const handler of handlers) {
+                handler(error);
+            }
+            return undefined;
+        }
+    };
+
+    composed.on = (name, handler) => {
+        if (name === 'error') {
+            handlers.push(handler);
+        }
+    };
+
+    return composed;
+};
+
+const inc = (x) => {
+    if (typeof x !== 'number') throw new Error('Argument must be a number!');
+    return x + 1;
+};
+const twice = (x) => {
+    if (typeof x !== 'number') throw new Error('Argument must be a number!');
+    return x * 2;
+};
+const cube = (x) => {
+    if (typeof x !== 'number') throw new Error('Argument must be a number!');
+    return x ** 3;
+};
+
+const throwError = (x) => {
+    if (typeof x !== 'number') throw new Error('Input must be a number!');
+    return x;
+};
+
+const composed = compose(throwError, inc, twice, cube);
+
+composed.on('error', (error) => {
+    console.error('Caught an error:', error.message);
+});
+
+console.log(composed(2)); // Результат: 17
+console.log(composed('two')); // Результат: undefined
+
--- Laba6/pipe.js
+++ Laba6/pipe.js
@@ -1,2 +1,28 @@
 'use strict';
 
+const pipe = (...fns) => {
+ for (const f of fns) {
+     if (typeof f !== 'function') {
+         throw new TypeError('Function must be a function');
+     }
+ }
+ return (x) => fns.reduce((v, func) => func(v), x);
+ }
+
+
+const inc = x => ++x;
+const twice = x => x * 2;
+const cube = x => x ** 3;
+
+//const f = pipe(inc, twice, cube);
+//const x = f(5);
+//console.log(x); // result: 1728
+
+//const f = pipe(inc, inc);
+//const x = f(7);
+//console.log(x); // result: 9
+
+const f = pipe(inc, 7, cube);
+//  throw new TypeError('Function must be a function');
+
+
