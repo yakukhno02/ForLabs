1098e32cb197af912f92564cb2d294fd31225c46
ykmsh <mayak.ol.02.10@gmail.com>
2024-12-10 10:56:36 +0200

    fix remove.js(lab7)

--- Laba7/remove.js
+++ Laba7/remove.js
@@ -2,14 +2,20 @@
 
 const removeElement = (array, item) => {
     const index = array.indexOf(item);
-    array.splice(index, 1);
+    if (index !== -1) array.splice(index, 1);
 };
 
 
-const array = [1, 2, 3, 4, 5, 6, 7];
-removeElement(array, 5);
-console.log(array);
+//const array = [1, 2, 3, 4, 5, 6, 7];
+//removeElement(array, 5);
+//console.log(array);
 
 // Result: [1, 2, 3, 4, 6, 7]
+
+const array = ['Kiev', 'Beijing', 'Lima', 'Saratov'];
+removeElement(array, 'Lima'); // remove 'Lima' from array
+removeElement(array, 'Berlin'); // do nothing
+console.log(array);
+// Result: ['Kiev', 'Beijing', 'Saratov']
 
 module.exports = { removeElement };
